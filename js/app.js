4074bfc8d427dee111e0e0c8a80c5bd323f53b09
Z <ouhassel@gmail.com>
2022-02-12 20:02:56 -0500

    download/upload cubes, no thin images

--- js/app.js
+++ js/app.js
@@ -7,13 +7,17 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 var material = [];
+var links = [];
 for(i=0;i<6;i++){
     loadTextures();
 }
+console.log(links);
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+controls.enablePan = false;
 
 camera.position.z = 1.75;
 controls.update();
@@ -28,26 +32,62 @@ animate();
 
 document.getElementById('go').addEventListener('click', () => {
     material = [];
+    links = [];
     for(i=0;i<6;i++){
         loadTextures();
     }
     cube.material = material;
 })
 
+document.getElementById('down').addEventListener('click', () => {
+    var chars = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'; 
+    var text = '';
+    for (var i = 0; i < 5; i++) {
+        var rand = Math.floor(Math.random() * chars.length);
+        text += chars.substring(rand,rand+1);
+    }
+    var a = document.createElement('a');
+    a.href = 'data:text/json, ["'+links.toString().replaceAll(',', '","')+'"]'
+    a.download = text+'.json';
+    a.click();
+});
+
+document.getElementById('up').addEventListener('click', () => {
+    material = [];
+    links = [];
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = function(){
+        var fr = new FileReader();
+        fr.onload = function(){
+            var toparse = fr.result.replaceAll('[','').replaceAll(']','').replaceAll('"','').replace(' ', '');
+            var array = toparse.split(',');
+            array.forEach((link)=>{
+                const loader = new THREE.TextureLoader();
+                links.push(link);
+                material.push(new THREE.MeshBasicMaterial({ map: loader.load(link) }));
+                cube.material = material;
+            })
+        }
+        fr.readAsText(input.files[0]);
+    }
+    input.click();
+});
+
 function loadTextures() {
     var chars = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'; 
     var text = '';
 
     for (var i = 0; i < 5; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        text += chars.substring(rnum,rnum+1);
+        var rand = Math.floor(Math.random() * chars.length);
+        text += chars.substring(rand,rand+1);
     }
 
     
     var image = new Image();
     image.src = 'https://i.imgur.com/' + text + '.jpg';;
     image.onload = function() {
-        if (this.width == 161) {
+        if (this.width == 161 && this.height/this.width > .2) {
             loadTextures();
         } else {
             var myHeaders = new Headers();
@@ -63,6 +103,7 @@ function loadTextures() {
                 .then(result => {
                     if(!JSON.parse(result).data.nsfw){
                         const loader = new THREE.TextureLoader();
+                        links.push(image.src);
                         material.push(new THREE.MeshBasicMaterial({ map: loader.load(image.src) }));
                     }
                 })
